5151ed6710c733b859ef5d53361d21219e711a97
busraayverdi <busraayverdi.95@gmail.com>
2021-06-20 22:25:22 +0300

    First commit

---
+++ src/layouts/CartSummart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CartSummart() {
+  return (
+    <div>
+      Sepet Özeti
+    </div>
+  )
+}
---
+++ src/layouts/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductList from "../pages/ProductList";
+import Navi from "./Navi";
+import Categories from "./Categories";
+
+export default function Dashboard() {
+          return <div>
+            <Navi/>
+            <Categories/>
+            <ProductList/>
+          </div>;
+}
